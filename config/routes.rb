1be71183ab3d421d78a9670032c6067b1c3a6481
kei.mori <selftracker1101@gmail.com>
2019-07-07 07:26:52 +0000

    login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   resources :users, only: [:index, :show, :new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
