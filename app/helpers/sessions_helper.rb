981a519badfb32230af538554057d558a9400c74
kei.mori <selftracker1101@gmail.com>
2019-07-09 12:10:12 +0000

    sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,3 +1,11 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
   
+  def logged_in?
+    #!はnot、current_userメソッドでログインユーザが見つかればtrue→false→true
+    #見つからなければnil→true→false
+    !!current_user
+  end
 end
