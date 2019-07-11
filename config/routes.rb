8961071b6f611f6c46789d14621762660d37d862
kei.mori <selftracker1101@gmail.com>
2019-07-11 13:01:25 +0000

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   resources :users, only: [:index, :show, :new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+  post 'logout', to: 'sessions#destroy'
 end
