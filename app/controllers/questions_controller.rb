1af51438f4045eb20d8b8a12e7a44daf4a1454a2
kei.mori <selftracker1101@gmail.com>
2019-07-15 09:42:42 +0000

    question controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,35 @@
+class QuestionsController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
+  def create
+    @question = current_user.questions.build(question_params)
+    if @question.save
+      flash[:success] = '問題を作成しました。'
+      redirect_to root_url
+    else
+     @questions = current_user.questions.order(id: :desc).page(params[:page])
+     flash.now[:danger] = '問題を作成できませんでした。'
+     render 'toppages/index'
+    end
+  end
+
+  def destroy
+    @question.destroy
+    flash[:success] = '問題を削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
+  
+  private
+  
+  def question_params
+    params.require(:question).permit(:title, :content, :answer)
+  end
+  
+  def correct_user
+    @question = current_user.questions.find_by(id: params[:id])
+    unless @question
+      redirect_to root_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   resources :users, only: [:index, :show, :new, :create, :destroy, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :questions, only: [:create, :destroy]
 end
