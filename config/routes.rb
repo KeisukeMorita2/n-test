d3716ff6eb3639a87aacd04c093405f6e731dfb3
kei.mori <selftracker1101@gmail.com>
2019-09-26 11:05:06 +0000

    フォロー機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  resources :users, only: [:index, :show, :new, :create, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :questions, only: [:create, :destroy, :index, :new, :show]
+  resources :relationships, only: [:create, :destroy]
 end
