9801896ff1044af7bdf7234d9d5f09ba6a78d97b
kei.mori <selftracker1101@gmail.com>
2019-10-19 03:32:52 +0000

    questionモデルにバリデーションを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :content, presence: true, length: { maximum: 255 }
   validates :answer, presence: true, length: { maximum: 255 }
 
